27acc2d52518f4807e6adee1df893111211d4525
TongLeen <tong-linn@outlook.com>
2025-03-24 22:04:17 +0800

    修复addText方法bug;新增添加列表和DataFrame功能

--- emailtools.py
+++ emailtools.py
@@ -7,36 +7,45 @@
 #   name:     emailtools                                                        #
 #   author:   TongLeen                                                          #
 #   date:     2025-3-20                                                         #
+#   version:  1.1.0                                                             #
 #                                                                               #
 #   python version:   3.12.2                                                    #
 #   dependency:                                                                 #
-#     - pillow                                                                  #
+#     - pillow (optional)                                                       #
+#     - pandas (optional)                                                       #
 #                                                                               #
 #################################################################################
 #                               how to use                                      #
 # 1. Instantiate a 'Email' as mail                                              #
 #   - use `mail = Email("mail_subject")` , it's the normal usage;               #
-#   - use `mail = Email.sequence("mail_subject", <content>, ...)`,              #
+#   - use `mail = Email.sequence("mail_subject", <content1>, <ctnt2>,...)`      #
 #       it's a convenient way to write a email.                                 #
 #       content could be a 'str' or 'PIL.Image.Image';                          #
 #       if str match markdown header, it will be inserted as a header:          #
 #           e.g. `## the header` -> `<h2>the header</h2>`                       #
+#       if pillow is avaliable, 'Image' can add to <content>                    #
+#       if pandas is avaliable, 'DataFrame' can add to <content>                #
 #                                                                               #
 # 2. Fill it with your content                                                  #
-#   For adding content after initialication, use <Email>.add* methods.          #
+#   For adding content after initialication, use <Email>.add* methods:          #
 #   - addHead       : add a header in email; level means the header-level;      #
 #   - addParagraph  : add a text paragraph                                      #
 #   - addText       : add a text as header or paragraph                         #
 #       if text match MD header, it will be converted to header; else paragraph #
-#   - addImage      : add a Image in email                                      #
+#   - addTable      : add a list or list[list] as table                         #
+#                                                                               #
+#   Following methods are avaliable when specific package exists:               #
+#   - addImage      : add a Image in email (avaliable when 'PIL' or 'pillow')   #
+#   - addDataFrame  : add a Dataframe as table (avaliable when 'pandas')        #
 #                                                                               #
 # 3. Instantiate a 'EmailServer' as emailServer                                 #
 #   - use `server = EmailServer(                                                #
 #       "smtp_server_addr", "local_nickname", "local_account", "local_passwd")` #
 #       it's the normal usage.                                                  #
-#       !! Caution DO NOT write your account and passwd in code !               #
+#       !! Caution DO NOT write your account and passwd in code                 #
 #   - use `server = EmainServer.initFromJson("config_file_path.json")`          #
 #       this avoids your account and passwd appearing in code.                  #
+#       !! The cfg file must be encoded by "utf-8"                              #
 #                                                                               #
 # 4. put the mail to emailServer                                                #
 #   - use `server.send(mail, {"revc_1_addr@xxx.com":"his_nickname", ...})`      #
@@ -79,25 +88,69 @@ CONFIG_TEMPLATE = """
 }
 """
 
-
+import base64
 import json
 import re
 
-from io import BytesIO
-from PIL import Image
-from typing import Self
 
+from io import BytesIO
 from smtplib import SMTP_SSL
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.image import MIMEImage
 from email.header import Header
+from typing import Self, Any, Sequence
+
+try:
+    from PIL import Image
+
+    _USING_PIL = True
+except ImportError:
+    _USING_PIL = False
+    print("Package 'PIL' not found. Relative functions not valid.")
+
+
+try:
+    import pandas as pd
+
+    _USING_PANDAS = True
+except ImportError:
+    _USING_PANDAS = False
+    print("Package 'pandas' not found. Relative functions not valid.")
 
 
 HEAD_RE = re.compile(r"^\W*#+ .*$")
+HTML_CSS = """
+table {
+  border-collapse: collapse;
+  border: 2px solid rgb(140 140 140);
+  font-family: sans-serif;
+  font-size: 0.8rem;
+  letter-spacing: 1px;
+}
+caption {
+  caption-side: top;
+  padding: 10px;
+  font-weight: bold;
+}
+thead,
+tfoot {
+  background-color: rgb(228 240 245);
+}
+th,
+td {
+  border: 1px solid rgb(160 160 160);
+  padding: 8px 10px;
+  text-align: center;
+}
+tbody > tr:nth-of-type(even) {
+  background-color: rgb(237 238 242);
+}
+"""
 
 
 class Email:
+
     def __init__(self, subject: str):
         self.root = MIMEMultipart()
         self.elementList: list[str] = []
@@ -119,33 +172,65 @@ class Email:
         if HEAD_RE.match(text):
             head = text.lstrip()
             level = head.find(" ") + 1
-            head = head[level + 1 :].lstrip()
+            head = head[level:].lstrip()
             self.addHead(head, level)
         else:
             self.addParagraph(text)
 
-    def addImage(self, img: Image.Image) -> None:
-        self.elementList.append(f'<p><img src="cid:img{self.imageCount}"/></p>')
-        buf = BytesIO()
-        img.save(buf, "png")
-        self.imageList.append((self.imageCount, buf.getvalue()))
-        self.imageCount += 1
+    def addTable(
+        self,
+        content: Sequence[Sequence],
+        head: Sequence | None = None,
+        caption: str | None = None,
+    ) -> None:
+        e = []
+        e.append('<table border="1" cellspacing="0" cellpadding="0">')
+        if caption is not None:
+            e.append(f"<caption>{caption}</caption>")
+        if head is not None:
+            h = [f"<td>{i}</td>" for i in head]
+            e.extend(["<thead><tr>", *h, "</tr></thead>"])
+
+        e.append("<tbody>")
+        for row in content:
+            r = [f"<td>{i}</td>" for i in row]
+            e.extend(["<tr>", *r, "</tr>"])
+        e.append("</tbody>")
+        e.append("</table>")
+
+        self.elementList.extend(e)
         return
 
     def _setSender(self, name: str, addr: str) -> None:
-        self.root["From"] = Header(f"{name} <{addr}>", "ascii")
+        self.root["From"] = Header(f"{self._encodeNikename(name)} <{addr}>", "ascii")
         return
 
     def _setReceivers(self, receivers: dict[str, str]) -> None:
         self.root["To"] = Header(
-            ";".join([f"{name} <{addr}>" for addr, name in receivers.items()]), "ascii"
+            ";".join(
+                [
+                    f"{self._encodeNikename(name)} <{addr}>"
+                    for addr, name in receivers.items()
+                ]
+            ),
+            "ascii",
         )
         return
+
+    @staticmethod
+    def _encodeNikename(name: str) -> str:
+        if name.isascii():
+            return name
+        else:
+            b = base64.b64encode(name.encode("utf-8"))
+            return f'"=?utf-8?B?{b.decode("ascii")}?="'
 
     def toBytes(self) -> bytes:
         self.root["Subject"] = Header(self.subject, "utf-8")
 
-        html = f'''<html><body>
+        html = f'''<html>
+        <head><style>{HTML_CSS}</style></head>
+        <body>
         {'\n'.join(self.elementList)}
         </body></html>'''
 
@@ -161,16 +246,41 @@ class Email:
         return self.root.as_bytes()
 
     @classmethod
-    def sequence(cls, subject: str, *items: str | Image.Image) -> Self:
+    def sequence(cls, subject: str, *items: Any) -> Self:
         mail = cls(subject)
         for i in items:
             if isinstance(i, str):
                 mail.addText(i)
-            elif isinstance(i, Image.Image):
+            elif isinstance(i, Sequence):
+                if bool(i) and isinstance(i[0], Sequence):
+                    mail.addTable(i)
+                else:
+                    mail.addTable((i,))
+            elif _USING_PIL and isinstance(i, Image.Image):
                 mail.addImage(i)
+            elif _USING_PANDAS and isinstance(i, pd.DataFrame):
+                mail.addDataFrame(i)
             else:
-                raise TypeError
+                raise TypeError(f"This type {type(i)} cannot append to email.")
         return mail
+
+    if _USING_PIL:
+
+        def addImage(self, img: Image.Image) -> None:
+            self.elementList.append(f'<p><img src="cid:img{self.imageCount}"/></p>')
+            buf = BytesIO()
+            img.save(buf, "png")
+            self.imageList.append((self.imageCount, buf.getvalue()))
+            self.imageCount += 1
+            return
+
+    if _USING_PANDAS:
+
+        def addDataFrame(self, df: pd.DataFrame, caption: str | None = None) -> None:
+            header = list(df.columns)
+            body = df.values.tolist()
+            self.addTable(body, head=header, caption=caption)
+            return
 
 
 class EmailServer:
@@ -203,7 +313,7 @@ class EmailServer:
 
     @classmethod
     def initFromJson(cls, filePath: str) -> Self:
-        with open(filePath, "r") as f:
+        with open(filePath, "r", encoding="utf-8") as f:
             cfg: dict = json.load(f)
         try:
             server = cls(cfg["host"], cfg["name"], cfg["addr"], cfg["key"])
